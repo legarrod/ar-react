e43d245f55cbcab01e1a43d161f84db52642a932
legarrod <samluis292@gmail.com>
2020-10-12 16:49:56 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<p>Esta es la imagen</p>
+			<a-scene
+				vr-mode-ui="enabled: false"
+				embedded
+				arjs="sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;"
+			>
+				<a-camera
+					gps-camera
+					rotation-reader
+					style={{ width: '200px', heigth: '200px' }}
+				></a-camera>
+				<a-entity
+					gltf-model="./assets/magnemite/scene.gltf"
+					rotation="0 180 0"
+					scale="0.15 0.15 0.15"
+					gps-entity-place="longitude: 4.449429; latitude: -75.783491;"
+					animation-mixer
+				/>
+			</a-scene>
+		</div>
+	);
 }
 
 export default App;
